8d307ddc949378d3d93da670026e446204e02b13
philip928lin <philip928lin@gmail.com>
2021-04-12 22:45:05 -0400

    update

--- HydroCNHS/Indicators.py
+++ HydroCNHS/Indicators.py
@@ -1,4 +1,5 @@
 import numpy as np 
+import pandas as pd
 
 class Indicator():
     """
@@ -6,10 +7,11 @@ class Indicator():
     r2  : Coefficient of determination
     rmse: Root mean square error
     NSE : Nash–Sutcliffe efficiency
+    iNSE: NSE with inverse transformed Q.
     CP  : Correlation of persistence
     RSR : RMSE-observations standard deviation ratio 
     KGE : Kling–Gupta efficiency
-    iKGE: Inverse Kling–Gupta efficiency
+    iKGE: KGE with inverse transformed Q.
     """
     # r = r
     # r2 = r2
@@ -21,6 +23,23 @@ class Indicator():
     
     def __init__(self) -> None:
         pass
+    
+    @staticmethod
+    def calIndicatorDF(xObv, ySim, IndicatorsList = None):
+        Dict = {"r"   : Indicator.r(xObv, ySim),
+                "r2"  : Indicator.r2(xObv, ySim),
+                "rmse": Indicator.rmse(xObv, ySim),
+                "NSE" : Indicator.NSE(xObv, ySim),
+                "iNSE": Indicator.iNSE(xObv, ySim),
+                "KGE" : Indicator.KGE(xObv, ySim),
+                "iKGE": Indicator.iKGE(xObv, ySim),
+                "CP"  : Indicator.CP(xObv, ySim),
+                "RSR" : Indicator.RSR(xObv, ySim)}
+        DF = pd.DataFrame(Dict)
+        if IndicatorsList is None:
+            return DF
+        else:
+            return DF.loc[:, IndicatorsList]
     
     @staticmethod
     def r(xObv, ySim):
@@ -76,6 +95,25 @@ class Indicator():
         return 1 - np.nansum((xObv-ySim)**2)/np.nansum((xObv-mu_xObv)**2) # Nash
     
     @staticmethod
+    def iNSE(xObv, ySim):
+        """Nash–Sutcliffe efficiency
+
+        Args:
+            xObv (Array): x or obv
+            ySim (Array): y or sim
+
+        Returns:
+            float
+        """
+        # Prevent dividing zero.
+        xObv = 1/(xObv + 0.01*np.nanmean(xObv))
+        ySim = 1/(ySim + 0.01*np.nanmean(ySim))
+        mu_ySim = np.nanmean(ySim); mu_xObv = np.nanmean(xObv)
+        sig_ySim = np.nanstd(ySim); sig_xObv = np.nanstd(xObv)
+        mu_xObv = np.nanmean(xObv)
+        return 1 - np.nansum((xObv-ySim)**2)/np.nansum((xObv-mu_xObv)**2) # Nash
+    
+    @staticmethod
     def CP(xObv, ySim):
         """Correlation of persistence
         
@@ -119,7 +157,8 @@ class Indicator():
     
     @staticmethod
     def iKGE(xObv, ySim):
-        """Kling–Gupta efficiency
+        """Kling–Gupta efficiency with inverse transformed flow.
+            https://www.fs.fed.us/nrs/pubs/jrnl/2015/nrs_2015_thirel_001.pdf
 
         Args:
             xObv (Array): x or obv
@@ -129,10 +168,8 @@ class Indicator():
             float
         """
         # Prevent dividing zero.
-        xObv[xObv == 0] = 0.0001
-        ySim[ySim == 0] = 0.0001
-        xObv = 1/xObv
-        ySim = 1/ySim
+        xObv = 1/(xObv + 0.01*np.nanmean(xObv))
+        ySim = 1/(ySim + 0.01*np.nanmean(ySim))
         mu_ySim = np.nanmean(ySim); mu_xObv = np.nanmean(xObv)
         sig_ySim = np.nanstd(ySim); sig_xObv = np.nanstd(xObv)
         return 1 - ((Indicator.r(xObv, ySim)-1)**2 + (sig_ySim/sig_xObv - 1)**2 + (mu_ySim/mu_xObv - 1)**2)**0.5
