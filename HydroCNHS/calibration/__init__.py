0de31781526285e72d7ae022b9e2ac31e6d1cce0
philip928lin <philip928lin@gmail.com>
2022-01-10 23:21:25 -0500

    update doc

--- HydroCNHS/calibration/__init__.py
+++ HydroCNHS/calibration/__init__.py
@@ -2,36 +2,35 @@ from .convertor import Convertor
 from .ga_deap import GA_DEAP
 def helper():
     print("(1) Code your customize evaluation function for GA algorithm with",
-          "certain arguments.\nEx:\ndef evaluation(individual, formatter,",
-          "info):\n\t# info = (cali_wd, current_generation, ith_individual)",
-          "\n\tRun model\n\tCalculate fitness\n\n\treturn (fitness,)\n\n(2)",
-          "Create calibration object from GA_DEAP() class",
+          "certain arguments.\nEx:\ndef evaluation(individual, info):\n\t# ",
+          "info = (cali_wd, current_generation, ith_individual, formatter, ",
+          "random_generator)\n\tRun model\n\tCalculate fitness\n\n\treturn "
+          "(fitness,)\n\n(2) Create calibration object from GA_DEAP() class",
           "given inputs and config.\n\n(3) Run GA.")
 
 def get_config_template():
     config = {
-        "min_or_max":         "min",
-        "pop_size":           100,    
+        "min_or_max":          "max",
+        "pop_size":            100,    
         "num_ellite":          1,     
-        "prob_cross":          0.8,  
+        "prob_cross":          0.5,  
         "prob_mut":            0.1,   
-        "stochastic":         False,  
+        "stochastic":          False,  
         "max_gen":             100,    
         "sampling_method":     "LHC",  
         "drop_record":         False,  
-        "paral_cores":         -2, 
-        "paral_verbose":        0,
+        "paral_cores":         -1, 
+        "paral_verbose":        1,
         "auto_save":           True,   
-        "print_level":         10,      
-        "plot":               True    
+        "print_level":          1,      
+        "plot":                True    
         }
     return config
 
 def get_inputs_template():
-    inputs = {
-        "par_name":    ["a","b","c"],     
-        "par_bound":   [[1,2],[1,2],[1,2]],      
-        "wd":          "working directory"} 
+    inputs = {"par_name":    ["a","b","c"],     
+            "par_bound":   [[1,2],[1,2],[1,2]],      
+            "wd":          "working directory"} 
     print("\nNote:\n Converter() can assist users",
           "to get inputs and formattor that can convert individual",
           "(1D array) back to Dataframe.")
--- docs/calibration.rst
+++ docs/calibration.rst
@@ -1,2 +1,63 @@
 Calibration
 ================
+
+Genetic algorithm calibration code outline
+-------------------------------
+
+.. code-block:: python
+
+    import HydroCNHS
+    import HydroCNHS.calibration as cali
+
+    # Assisting functions
+    cali.helper()
+    cali.get_config_template()
+    cali.get_inputs_template()
+
+    def evaluation(individual, info):
+        (cali_wd, current_generation, ith_individual,
+        formatter, random_generator) = info
+        # Run model.
+        # Calculate fitness. 
+        # E.g.,
+        fitness = sum(individual)
+        return (fitness,)
+
+    config = {'min_or_max': 'max',
+            'pop_size': 200,
+            'num_ellite': 1,
+            'prob_cross': 0.5,
+            'prob_mut': 0.1,
+            'stochastic': False,
+            'max_gen': 100,
+            'sampling_method': 'LHC',
+            'drop_record': False,
+            'paral_cores': -1,
+            'paral_verbose': 1,
+            'auto_save': True,
+            'print_level': 1,
+            'plot': True}
+
+    cali_inputs = {"par_name":    ["a","b","c"],     
+                "par_bound":   [[1,2],[1,2],[1,2]],      
+                "wd":          "working directory"} 
+
+    seed = 3
+    rn_gen = HydroCNHS.create_rn_gen(seed)  # Optional
+    ga = cali.GA_DEAP(evaluation, rn_gen)
+    ga.set(cali_inputs, config, name="Cali_example")
+    ga.run()
+    ga.run_individual(ga.solution)  # Output performance (.txt) of solution.
+    summary = ga.summary
+    print(summary)
+
+
+
+Converter
+---------
+
+
+
+
+TRB calibration example
+-----------------------
