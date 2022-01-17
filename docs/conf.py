5294a4bc001e4dc002dc7dbd412b9b288bfc3953
philip928lin <philip928lin@gmail.com>
2022-01-17 14:02:15 -0500

    update doc

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ author = 'Chung-Yi Lin'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ["sphinx.ext.autodoc",
-              "nbsphinx",
+              #"nbsphinx",
               'sphinx.ext.autosectionlabel']
 
 # Add any paths that contain templates here, relative to this directory.
